b8793407ab2e9c00206e88c93d690c4dca954ef3
pradipchougule0811 <pradipchougule0811@gmail.com>
2025-04-06 11:12:35 +0530

    First Program

--- src/com/pradip/MergeSortedArray.java
+++ src/com/pradip/MergeSortedArray.java
@@ -1,5 +1,9 @@
 package com.pradip;
 
+import java.util.Arrays;
+
+import static com.pradip.MergeSortedArray.merge;
+
 /***
  * You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and
  * two integers m and n, representing the number of elements in nums1 and nums2 respectively.
@@ -41,4 +45,23 @@ package com.pradip;
  * Follow up: Can you come up with an algorithm that runs in O(m + n) time?
  */
 public class MergeSortedArray {
+    public static void main(String[] args) {
+        int[] nums1 ={0};//{1,2,3,0,0,0};
+        int m = 0;
+        int[] nums2 ={1};//{2,5,6};
+        int n = 1;
+        merge(nums1,m,nums2,n);
+        for (int i = 0; i < (m+n) ; i++) {
+            System.out.print("   "+nums1[i]);
+        }
+    }
+    public static void  merge(int[] nums1, int m, int[] nums2, int n) {
+        int i1 = m + n;
+        for (int i = 0; i< i1; i++){
+            if(!(i<m)){
+                nums1[i] = nums2[i-m];
+            }
+        }
+        Arrays.sort(nums1);
+    }
 }
